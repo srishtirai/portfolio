c3bdefa192e9fc6a2aa51603b61a6dc5af890341
srishtirai <srishticrai@gmail.com>
2025-03-06 02:33:26 -0500

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Chatbot from "@/components/Chatbot";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "My Portfolio",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
+        <Analytics/>
         <NavBar />
         <main className="mx-[10px] mt-[80px] sm:mx-[60px] sm:mt-[100px] md:mx-[80px] md:mt-[120px]">{children}</main>
         <Chatbot />
