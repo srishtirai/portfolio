291a4aa533d6ac5b8f94e70bf343abbd6e76a70d
srishtirai <srishticrai@gmail.com>
2025-03-06 16:52:34 -0500

    Added few tempororay images for project details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="text-center text-primary text-xs sm:text-sm py-6 bg-accent bg-opacity-30">
       <p>
-        Built with <span className="text-primary">❤️</span> and lots of <strong>JavaScript</strong>.
+        Built with <span className="text-primary">❤️</span> and lots of <strong>TypeScript</strong>.
       </p>
       <p>
         Copyright © 2025 <strong>Srishti Rai</strong>
