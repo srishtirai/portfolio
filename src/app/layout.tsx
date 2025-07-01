8288967ea91b8cfd74d7f753502f1edb82f0c818
srishtirai <srishticrai@gmail.com>
2025-06-30 22:14:54 -0400

    Removed chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-import Chatbot from "@/components/Chatbot";
+// import Chatbot from "@/components/Chatbot";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
@@ -23,7 +23,7 @@ export default function RootLayout({
         <SpeedInsights/>
         <NavBar />
         <main className="mx-[10px] mt-[20px] sm:mx-[60px] sm:mt-[100px] md:mx-[80px] md:mt-[120px]">{children}</main>
-        <Chatbot />
+        {/* <Chatbot /> */}
         <Footer />
       </body>
     </html>
